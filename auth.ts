c5aa1b86422a127637748042b4733928aa281b5e
Felipe Rosas <contato@eufelipe.com>
2024-05-19 12:38:00 -0300

    chore: setup next auth

---
+++ auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+
+import Credentials from "next-auth/providers/credentials";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  providers: [Credentials],
+});
